b56ce524baf87f7a014b14d6b30130e12ee125f4
Yash Rastogi <109573905+Yrastogi@users.noreply.github.com>
2024-07-10 08:52:12 +0000

    new

---
+++ newcode/yash.js
@@ -0,0 +1,8 @@
+let score="321fsadfs";
+console.log(typeof score);
+console.log(typeof(score));
+let valueInnumber=Number(score);
+ console.log(typeof(valueInnumber));
+ let isLoggedIn =1;
+ let booleanIsLoggedIn=Boolean(isLoggedIn);
+ console.log(booleanIsLoggedIn);
